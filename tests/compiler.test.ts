1a2cdaed3af03a737f03a8212258446fea25532b
Moros1138 <moros1138@gmail.com>
2023-05-02 05:42:41 -0400

    formatting

--- tests/compiler.test.ts
+++ tests/compiler.test.ts
@@ -7,16 +7,16 @@ describe('Compiler and Support Functions', () =>
 {
     it('writeSourceFile creates temporary folder', async () =>
     {
-        const result = await writeSourceFile('some source');
+        const result = await writeSourceFile("some source");
 
-        expect(result).toHaveProperty('tmpObject');
-        expect(result).toHaveProperty('tmpName');
+        expect(result).toHaveProperty("tmpObject");
+        expect(result).toHaveProperty("tmpName");
 
-        expect(result.tmpObject).toHaveProperty('name');
-        expect(result.tmpObject).toHaveProperty('removeCallback');
+        expect(result.tmpObject).toHaveProperty("name");
+        expect(result.tmpObject).toHaveProperty("removeCallback");
     });
 
-    it('emscripten is installed in the current environment', async () =>
+    it("emscripten is installed in the current environment", async () =>
     {
         const result = await execute('emcc -v');
 
@@ -26,14 +26,14 @@ describe('Compiler and Support Functions', () =>
         expect(result.stderr).toMatch(/Emscripten/i);
     });
 
-    it('compile "Hello, World" C source file', async () =>
+    it("compile \"Hello, World\" C source file", async () =>
     {
         const result = await compile('#include <stdio.h>\nint main(int argc, char* argv[])\n{\nprintf("Hello, World\\n");\nreturn 0;\n}\n');
 
         expect(result.compiledSuccessfully).toBe(true);
     });
 
-    it('compile olcExampleProgram.cpp', async () =>
+    it("compile olcExampleProgram.cpp", async () =>
     {
         // read olcExampleProgram.cpp
         let source: string = readFile(path.resolve(__dirname, 'third_party', 'olcPixelGameEngine', 'olcExampleProgram.cpp'));
