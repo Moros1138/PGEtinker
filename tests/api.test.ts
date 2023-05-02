22363c8f379670174c574fa50a4687c9634f61c2
Moros1138 <moros1138@gmail.com>
2023-05-02 02:26:22 -0400

    create test: PGEtinker API -> GET /api/default-code

--- tests/api.test.ts
+++ tests/api.test.ts
@@ -2,9 +2,17 @@ import { describe, it } from "vitest";
 import supertest from "supertest";
 import { app } from "../lib/expressApp";
 
-describe.todo("PGEtinker API (unimplemented)", () =>
+describe("PGEtinker API", () =>
 {
-    it.todo("gets default code");
+    it("GET /api/default-code - gets default code", async () =>
+    {
+        await supertest(app).get('/api/default-code')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(/OLC_PGE_APPLICATION/i)
+            .expect(200);
+    });
+
     it.todo("compiles a test program");
     it.todo("shares a test program");
     it.todo("gets header files for monaco model");
