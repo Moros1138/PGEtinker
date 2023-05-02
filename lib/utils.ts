e083f3c5541405740f4cf9c2a7255583c01d1426
Moros1138 <moros1138@gmail.com>
2023-05-01 23:24:22 -0400

    utils.execute

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,34 @@
+import { exec } from 'child_process';
 import * as fs from 'node:fs';
+
 export const __dirname: string = process.env.INIT_CWD as string;
+
 export const fileExists = fs.existsSync;
+
+export const execute = (command: string, options: any = {}) : Promise<any> =>
+{
+    // i solemnly swear that i am up to no good, promise!
+    return new Promise((resolve, reject) =>
+    {
+        try
+        {
+            // stop making excuses and execute
+            exec(command, options, (error, stdout, stderr) =>
+            {
+                // reject if killed
+                if(error?.killed)
+                {
+                    reject({ killed: true });
+                    return;
+                }
+
+                // resolve results
+                resolve({stdout: stdout, stderr: stderr});
+            });
+        }
+        catch(e)
+        {
+            reject(e);
+        }
+    });
+}
