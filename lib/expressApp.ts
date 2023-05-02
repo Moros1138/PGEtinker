6dab99ac91052a0348cc0752d90679f9472cebca
Moros1138 <moros1138@gmail.com>
2023-05-02 02:25:09 -0400

    GET /api/default-code (implemented)

--- lib/expressApp.ts
+++ lib/expressApp.ts
@@ -1,8 +1,16 @@
 import express, { Express, Request, Response } from 'express';
+import { __dirname, readFile } from './utils';
+import path from 'path';
 
 export const app : Express = express();
 
 app.use(express.json());
+
+app.get('/api/default-code', (_: Request, res: Response) =>
+{
+    let defaultCode = readFile(path.resolve(__dirname, 'examples', 'default.cpp'));
+    res.status(200).json({code: defaultCode});
+});
 
 app.get("/api/hello", (_: Request, res: Response) => res.json({ hello: "world" }));
 // app.get("/api/does-not-exist", (_: Request, res: Response) => res.json({ hello: "world" }));
