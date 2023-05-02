6da9c744503dfe858d3c18b91b3432adc4a9a685
Moros1138 <moros1138@gmail.com>
2023-05-01 23:37:15 -0400

    create test: compile "Hello, World" C source file

--- tests/compiler.test.ts
+++ tests/compiler.test.ts
@@ -1,4 +1,5 @@
 import { describe, expect, it } from "vitest";
+import { compile } from "../lib/compiler";
 import { __dirname, execute, readFile, writeSourceFile } from "../lib/utils";
 describe('Compiler and Support Functions', () =>
 {
@@ -21,6 +22,13 @@ describe('Compiler and Support Functions', () =>
         expect(result).toHaveProperty('stderr');
 
         expect(result.stderr).toMatch(/Emscripten/i);
+    });
+
+    it('compile "Hello, World" C source file', async () =>
+    {
+        const result = await compile('#include <stdio.h>\nint main(int argc, char* argv[])\n{\nprintf("Hello, World\\n");\nreturn 0;\n}\n');
+
+        expect(result.compiledSuccessfully).toBe(true);
     });
 
 });
