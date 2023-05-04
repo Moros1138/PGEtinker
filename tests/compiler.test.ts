ef2134704504a86ae6d24b9800829bb5d7f06b0b
Moros1138 <moros1138@gmail.com>
2023-05-04 02:22:01 -0400

    reorganize tests to cut down on time related issues.

--- tests/compiler.test.ts
+++ tests/compiler.test.ts
@@ -5,42 +5,48 @@ import path from "path";
 
 describe('Compiler and Support Functions', () =>
 {
-    it('writeSourceFile creates temporary folder', async () =>
+    describe('Support Functions and Emscripten', () =>
     {
-        const result = await writeSourceFile("some source");
-        expect(fileExists(`${result}.cpp`)).toBe(true);
+        it('writeSourceFile creates temporary folder', async () =>
+        {
+            const result = await writeSourceFile("some source");
+            expect(fileExists(`${result}.cpp`)).toBe(true);
+        });
+
+        it("emscripten is installed in the current environment", async () =>
+        {
+            const result = await execute('emcc -v');
+
+            expect(result).toHaveProperty('stdout');
+            expect(result).toHaveProperty('stderr');
+
+            expect(result.stderr).toMatch(/Emscripten/i);
+        });
     });
 
-    it("emscripten is installed in the current environment", async () =>
+    describe("Compiler Tests", () =>
     {
-        const result = await execute('emcc -v');
+        it("compile \"Hello, World\" C source file", async () =>
+        {
+            const result = await compile('#include <stdio.h>\nint main(int argc, char* argv[])\n{\nprintf("Hello, World\\n");\nreturn 0;\n}\n');
 
-        expect(result).toHaveProperty('stdout');
-        expect(result).toHaveProperty('stderr');
+            expect(result.compiledSuccessfully).toBe(true);
+        });
 
-        expect(result.stderr).toMatch(/Emscripten/i);
-    });
+        it("compile olcExampleProgram.cpp", async () =>
+        {
+            // read olcExampleProgram.cpp
+            let source = readFile(path.resolve(__dirname, 'third_party', 'olcPixelGameEngine', 'olcExampleProgram.cpp'));
 
-    it("compile \"Hello, World\" C source file", async () =>
-    {
-        const result = await compile('#include <stdio.h>\nint main(int argc, char* argv[])\n{\nprintf("Hello, World\\n");\nreturn 0;\n}\n');
+            const result = await compile(source.toString());
 
-        expect(result.compiledSuccessfully).toBe(true);
-    });
+            expect(result.compiledSuccessfully).toBe(true);
+        }, 10000);
 
-    it("compile olcExampleProgram.cpp", async () =>
-    {
-        // read olcExampleProgram.cpp
-        let source: string = readFile(path.resolve(__dirname, 'third_party', 'olcPixelGameEngine', 'olcExampleProgram.cpp'));
-
-        const result = await compile(source);
-
-        expect(result.compiledSuccessfully).toBe(true);
-    }, 10000);
-
-    it("compile \"Hello, World\" C source file with hacking include attempt", async () =>
-    {
-        const result = await compile('#include <stdio.h>\n#include "/etc/hostname"\n#include "/etc/passwd"\nint main(int argc, char* argv[])\n{\nprintf("Hello, World\\n");\nreturn 0;\n}\n');
-        expect(result.stderr).toMatch(/no absolute or relative includes please/);
+        it("compile \"Hello, World\" C source file with hacking include attempt", async () =>
+        {
+            const result = await compile('#include <stdio.h>\n#include "/etc/hostname"\n#include "/etc/passwd"\nint main(int argc, char* argv[])\n{\nprintf("Hello, World\\n");\nreturn 0;\n}\n');
+            expect(result.stderr).toMatch(/no absolute or relative includes please/);
+        });
     });
 });
