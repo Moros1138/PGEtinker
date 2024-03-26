e131a4806ae1cace10a0dfa66a60dc0c66e5f9ae
Moros Smith <moros1138@gmail.com>
2024-03-26 16:10:09 -0400

    indicate which unit is being built

--- index.js
+++ index.js
@@ -46,24 +46,24 @@ const app = express();
 const port = 3000
 
 app.use(morgan('tiny'))
-
 app.use(express.static('public_html'))
-
 app.use(express.json());
 
 app.post("/compile", async(req, res) =>
 {
+    // bail out if we haven't been provided some code to process
     if(typeof req.body.code === 'undefined')
     {
         res.status(400).send({code: 400, message: "missing required parameters"});
         return;
     }
     
+    // break the code up into an array
     let code = req.body.code.split("\n");
     let libraries = [];
     let errors    = [];
 
-    // perform source code filtering
+    // line by line code processing and filtering
     for(i = 0; i < code.length; i++)
     {
         // filter include macros with an absolute path, naughty naughty
@@ -97,17 +97,21 @@ app.post("/compile", async(req, res) =>
         }
     }
     
+    // if we've detected errors at this point, let's bail out here, without invoking the compiler
     if(errors.length > 0)
     {
-        console.log("awww fuck!");
+        res.status(400).send({code: 400, message: "missing required parameters", stdout: "", stderr: errors.join("\n")});
+        return;
     }
 
-    console.log(code.join("\n"), libraries.join(" "));
-    
+    // we made it here, time to try the compiler
     const compiler = exec("./scripts/build-local.sh " + libraries.join(" "), (error, stdout, stderr) =>
     {
-        console.log(stdout, stderr);
-        res.send({ message: "compile"});
+        res.send({
+            code: 200,
+            stdout: stdout,
+            stderr: stderr
+        });
     });
     
     // feed the code to the script, via pipe
@@ -118,5 +122,6 @@ app.post("/compile", async(req, res) =>
 
 app.listen(port, () =>
 {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`PGEtinker Server listening on port ${port}`);
 });
+
--- scripts/build-pge-cache.sh
+++ scripts/build-pge-cache.sh
@@ -7,47 +7,58 @@ fi
 
 # build PGE and all pre-buildable extensions/utils
 if [ ! -e ./cache/third-party/olcPixelGameEngine.o ] ; then
+    echo Building olcPixelGameEngine.o
     em++ -c -std=c++20 -I./third-party/olcPixelGameEngine -I./third-party/olcPixelGameEngine/extensions -I./third-party/olcPixelGameEngine/utilities -I./third-party/olcSoundWaveEngine ./third-party/olcPixelGameEngine/olcPixelGameEngine.cpp -o ./cache/third-party/olcPixelGameEngine.o
 fi
 
 if [ ! -e ./cache/third-party/olcPGEX_Graphics2D.o ] ; then
+    echo Building olcPGEX_Graphics2D.o
     em++ -c -std=c++20 -I./third-party/olcPixelGameEngine -I./third-party/olcPixelGameEngine/extensions -I./third-party/olcPixelGameEngine/utilities -I./third-party/olcSoundWaveEngine ./third-party/olcPixelGameEngine/extensions/olcPGEX_Graphics2D.cpp -o ./cache/third-party/olcPGEX_Graphics2D.o
 fi
 
 if [ ! -e ./cache/third-party/olcPGEX_Graphics3D.o ] ; then
+    echo Building olcPGEX_Graphics3D.o
     em++ -c -std=c++20 -I./third-party/olcPixelGameEngine -I./third-party/olcPixelGameEngine/extensions -I./third-party/olcPixelGameEngine/utilities -I./third-party/olcSoundWaveEngine ./third-party/olcPixelGameEngine/extensions/olcPGEX_Graphics3D.cpp -o ./cache/third-party/olcPGEX_Graphics3D.o
 fi
 
 if [ ! -e ./cache/third-party/olcPGEX_PopUpMenu.o ] ; then
+    echo Building olcPGEX_PopUpMenu.o
     em++ -c -std=c++20 -I./third-party/olcPixelGameEngine -I./third-party/olcPixelGameEngine/extensions -I./third-party/olcPixelGameEngine/utilities -I./third-party/olcSoundWaveEngine ./third-party/olcPixelGameEngine/extensions/olcPGEX_PopUpMenu.cpp -o ./cache/third-party/olcPGEX_PopUpMenu.o
 fi
 
 if [ ! -e ./cache/third-party/olcPGEX_QuickGUI.o ] ; then
+    echo Building olcPGEX_QuickGUI.o
     em++ -c -std=c++20 -I./third-party/olcPixelGameEngine -I./third-party/olcPixelGameEngine/extensions -I./third-party/olcPixelGameEngine/utilities -I./third-party/olcSoundWaveEngine ./third-party/olcPixelGameEngine/extensions/olcPGEX_QuickGUI.cpp -o ./cache/third-party/olcPGEX_QuickGUI.o
 fi
 
 if [ ! -e ./cache/third-party/olcPGEX_RayCastWorld.o ] ; then
+    echo Building olcPGEX_RayCastWorld.o
     em++ -c -std=c++20 -I./third-party/olcPixelGameEngine -I./third-party/olcPixelGameEngine/extensions -I./third-party/olcPixelGameEngine/utilities -I./third-party/olcSoundWaveEngine ./third-party/olcPixelGameEngine/extensions/olcPGEX_RayCastWorld.cpp -o ./cache/third-party/olcPGEX_RayCastWorld.o
 fi
 
 if [ ! -e ./cache/third-party/olcPGEX_Sound.o ] ; then
+    echo Building olcPGEX_Sound.o
     em++ -c -std=c++20 -I./third-party/olcPixelGameEngine -I./third-party/olcPixelGameEngine/extensions -I./third-party/olcPixelGameEngine/utilities -I./third-party/olcSoundWaveEngine ./third-party/olcPixelGameEngine/extensions/olcPGEX_Sound.cpp -o ./cache/third-party/olcPGEX_Sound.o
 fi
 
 if [ ! -e ./cache/third-party/olcPGEX_SplashScreen.o ] ; then
+    echo Building olcPGEX_SplashScreen.o
     em++ -c -std=c++20 -I./third-party/olcPixelGameEngine -I./third-party/olcPixelGameEngine/extensions -I./third-party/olcPixelGameEngine/utilities -I./third-party/olcSoundWaveEngine ./third-party/olcPixelGameEngine/extensions/olcPGEX_SplashScreen.cpp -o ./cache/third-party/olcPGEX_SplashScreen.o
 fi
 
 if [ ! -e ./cache/third-party/olcPGEX_TransformedView.o ] ; then
+    echo Building olcPGEX_TransformedView.o
     em++ -c -std=c++20 -I./third-party/olcPixelGameEngine -I./third-party/olcPixelGameEngine/extensions -I./third-party/olcPixelGameEngine/utilities -I./third-party/olcSoundWaveEngine ./third-party/olcPixelGameEngine/extensions/olcPGEX_TransformedView.cpp -o ./cache/third-party/olcPGEX_TransformedView.o
 fi
 
 if [ ! -e ./cache/third-party/olcPGEX_Wireframe.o ] ; then
+    echo Building olcPGEX_Wireframe.o
     em++ -c -std=c++20 -I./third-party/olcPixelGameEngine -I./third-party/olcPixelGameEngine/extensions -I./third-party/olcPixelGameEngine/utilities -I./third-party/olcSoundWaveEngine ./third-party/olcPixelGameEngine/extensions/olcPGEX_Wireframe.cpp -o ./cache/third-party/olcPGEX_Wireframe.o
 fi
 
 # build SWE
 if [ ! -e ./cache/third-party/olcSoundWaveEngine.o ] ; then
+    echo Building olcSoundWaveEngine.o
     em++ -c -std=c++20 -I./third-party/olcPixelGameEngine -I./third-party/olcPixelGameEngine/extensions -I./third-party/olcPixelGameEngine/utilities -I./third-party/olcSoundWaveEngine ./third-party/olcSoundWaveEngine/olcSoundWaveEngine.cpp -o ./cache/third-party/olcSoundWaveEngine.o
 fi
 
