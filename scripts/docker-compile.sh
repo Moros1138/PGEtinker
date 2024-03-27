5e5cbb3b3bae6c0e493ac406a8bc5755c8dcae56
Moros Smith <moros1138@gmail.com>
2024-03-27 01:07:22 -0400

    dockerized build scripts

---
+++ scripts/docker-compile.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+# 
+# $1 = path of the temporary working directory
+# 
+
+docker run --rm \
+    -v $1:/src/tmp \
+    -v $(pwd)/third-party:/src/third-party \
+    -v $(pwd)/cache/docker-emscripten:/emsdk/upstream/emscripten/cache \
+    -u $(id -u):$(id -g) \
+    pgetinker/builder \
+    em++ \
+    -O2 \
+    -c \
+    -I/src/third-party/olcPixelGameEngine \
+    -I/src/third-party/olcPixelGameEngine/extensions \
+    -I/src/third-party/olcPixelGameEngine/utilities \
+    -I/src/third-party/olcSoundWaveEngine \
+    /src/tmp/pgetinker.cpp -o /src/tmp/pgetinker.o
