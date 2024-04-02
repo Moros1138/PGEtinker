6f174cbc40a131e5809d458a7e9e9cf846515ada
Moros Smith <moros1138@gmail.com>
2024-04-01 20:58:21 -0400

    delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-
-let config = {};
-
-config.hostname  = process.env.HOST       || "localhost";
-config.port      = process.env.PORT       || 3000;
-config.buildWith = process.env.BUILD_WITH || "local";
-
-module.exports = config;
