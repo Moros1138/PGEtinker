9d29ed7d0ef31ec92573cace1578b3e8e9cab883
Moros Smith <moros1138@gmail.com>
2024-04-04 02:03:23 -0400

    separate compile function from the api handler

--- src/server/lib/ApiHandler.js
+++ src/server/lib/ApiHandler.js
@@ -55,196 +55,223 @@ const compileScript = process.env.COMPILE_SCRIPT || "local";
 /** @type {RegExp} - regex pattern to detect absolute path in include/import macros */
 const absolutePathRegex = /^\s*#\s*i(nclude|mport)(_next)?\s+["<]((\.{1,2}|\/)[^">]*)[">]/;
 
-function Compile(request, response)
+/**
+ * attempts to compile the provided code, rejects when there are errors, resolve on success
+ * @param {string} code 
+ * @returns {Promise<any>}
+ */
+function Compile(code)
 {
-    // bail out if we haven't been provided some code to process
-    if(typeof request.body.code === "undefined")
+    return new Promise((resolve, reject) =>
     {
-        response.status(400)
-                .send({
-                    message: "missing required parameters"
-                });
-        return;
-    }
+        /** @type {Array<string>} */
+        let linesOfCode = code.split("\n");
+        
+        /** @type {Array<string>} */
+        let errors    = [];
+        
+        /** @type {Array<string>} */
+        let libraries = [];
 
-    /** @type {Array<string>} */
-    let linesOfCode = request.body.code.split("\n");
-    
-    /** @type {Array<string>} */
-    let errors    = [];
-    
-    /** @type {Array<string>} */
-    let libraries = [];
-
-    // line by line code processing and filtering
-    for(let i = 0; i < linesOfCode.length; i++)
-    {
-        // filter include macros with an absolute path, naughty naughty
-        if(absolutePathRegex.test(linesOfCode[i]))
+        // line by line code processing and filtering
+        for(let i = 0; i < linesOfCode.length; i++)
         {
-            errors.push(`/pgetinker.cpp:${i+1}:1: error: absolute and relative includes are not allowed.`);
-            
-            // no further processing required for this line, NEXT!!
-            continue;
-        }
-
-        // filter macros to detect implementation #define
-        if(linesOfCode[i].includes("#define"))
-        {
-            /** @type {boolean} */
-            let foundImplementationMacro = false;
-            
-            for(let j = 0; j < libraryMacroToObject.length; j++)
+            // filter include macros with an absolute path, naughty naughty
+            if(absolutePathRegex.test(linesOfCode[i]))
             {
-                if(linesOfCode[i].includes(libraryMacroToObject[j].define))
-                {
-                    // blank the line
-                    linesOfCode[i] = "";
-
-                    // indicated that we use this library
-                    libraries.push(libraryMacroToObject[j].objectFile);
-
-                    foundImplementationMacro = true;
-                    break;
-                }
+                errors.push(`/pgetinker.cpp:${i+1}:1: error: absolute and relative includes are not allowed.`);
+                
+                // no further processing required for this line, NEXT!!
+                continue;
             }
 
-            // thank you, NEXT!!
-            if(foundImplementationMacro)
-                continue;
-        }
-    }
+            // filter macros to detect implementation #define
+            if(linesOfCode[i].includes("#define"))
+            {
+                /** @type {boolean} */
+                let foundImplementationMacro = false;
+                
+                for(let j = 0; j < libraryMacroToObject.length; j++)
+                {
+                    if(linesOfCode[i].includes(libraryMacroToObject[j].define))
+                    {
+                        // blank the line
+                        linesOfCode[i] = "";
 
-    // bail if we have errors here, no need to invoke the compiler
-    if(errors.length > 0)
-    {
-        response.status(400)
-                .send({
-                    stdout: "",
-                    stderr: errors.join("\n")
-                });
-        return;
-    }
+                        // indicated that we use this library
+                        libraries.push(libraryMacroToObject[j].objectFile);
 
-    // create the workspace and save it's location
-    const workspaceDirectory = mktemp.createDirSync(join(tmpdir(), "pgetinker-XXXXXX"));
-    const sourceFile = join(workspaceDirectory, "pgetinker.cpp");
-    const outputFile = join(workspaceDirectory, "pgetinker.html");
+                        foundImplementationMacro = true;
+                        break;
+                    }
+                }
 
-    // make sure the workspace exists.
-    if(!existsSync(workspaceDirectory))
-    {
-        response.status(500)
-                .send({
-                    statusCode: 500,
-                    message: "Could not create workspace."
-                });
-        return;
-    }
-
-    writeFileSync(sourceFile, linesOfCode.join("\n"));
-
-    if(!existsSync(sourceFile))
-    {
-        response.status(500)
-                .send({
-                    statusCode: 500,
-                    message: "Could not create source file."
-                });
-        
-        rmSync(workspaceDirectory, { force: true, recursive: true });
-        return;
-    }
-    
-    let compileCommand = [
-        `scripts/${compileScript}-compile.sh`,
-        workspaceDirectory,
-    ];
-
-    let linkCommand = [
-        `scripts/${compileScript}-link.sh`,
-        workspaceDirectory,
-        ...libraries,
-    ];
-
-    function FilterOutput(text)
-    {
-        text = text.replaceAll("/src/tmp", "");
-        text = text.replaceAll("/src/third-party/olcPixelGameEngine", "");
-        text = text.replaceAll("/src/third-party/olcPixelGameEngine/extensions", "");
-        text = text.replaceAll("/src/third-party/olcPixelGameEngine/utilities", "");
-        text = text.replaceAll("/src/third-party/olcSoundWaveEngine", "");
-        text = text.replaceAll("/src/cache/third-party", "");
-        
-        text = text.replaceAll(workspaceDirectory, "");
-        text = text.replaceAll("./third-party/olcPixelGameEngine", "");
-        text = text.replaceAll("./third-party/olcPixelGameEngine/extensions", "");
-        text = text.replaceAll("./third-party/olcPixelGameEngine/utilities", "");
-        text = text.replaceAll("./third-party/olcSoundWaveEngine", "");
-        
-        text = text.split("\n");
-        let retval = [];
-        for(let i = 0; i < text.length; i++)
-        {
-            if(text[i].indexOf("/pgetinker.cpp") === 0)
-                retval.push(text[i]);
+                // thank you, NEXT!!
+                if(foundImplementationMacro)
+                    continue;
+            }
         }
         
-        return retval.join("\n");
-    }
-
-    exec(compileCommand.join(" "), (error, stdout, stderr) =>
-    {
-        if(error)
+        // bail if we have errors here, no need to invoke the compiler
+        if(errors.length > 0)
         {
-            response.status(400)
-                    .send({
-                        stdout: FilterOutput(stdout),
-                        stderr: FilterOutput(stderr),
-                    });
-            
-            rmSync(workspaceDirectory, { force: true, recursive: true});
+            reject({
+                code: 400,
+                stdout: "",
+                stderr: errors.join("\n")
+            });
+            return;
+        }
+
+        // create the workspace and save it's location
+        const workspaceDirectory = mktemp.createDirSync(join(tmpdir(), "pgetinker-XXXXXX"));
+        const sourceFile = join(workspaceDirectory, "pgetinker.cpp");
+        const outputFile = join(workspaceDirectory, "pgetinker.html");
+
+        // make sure the workspace exists.
+        if(!existsSync(workspaceDirectory))
+        {
+            reject({
+                code: 500,
+                message: "Could not create workspace."
+            });
             return;
         }
         
-        exec(linkCommand.join(" "), (error, stdout, stderr) =>
+        // try to create the source file and fill it with linesOfCode
+        writeFileSync(sourceFile, linesOfCode.join("\n"));
+
+        if(!existsSync(sourceFile))
+        {
+            reject({
+                code: 500,
+                message: "Could not create source file."
+            })
+            
+            // cleanup
+            rmSync(workspaceDirectory, { force: true, recursive: true });
+            return;
+        }
+
+        let compileCommand = [
+            `scripts/${compileScript}-compile.sh`,
+            workspaceDirectory,
+        ];
+    
+        let linkCommand = [
+            `scripts/${compileScript}-link.sh`,
+            workspaceDirectory,
+            ...libraries,
+        ];
+    
+        function FilterOutput(text)
+        {
+            text = text.replaceAll("/src/tmp", "");
+            text = text.replaceAll("/src/third-party/olcPixelGameEngine", "");
+            text = text.replaceAll("/src/third-party/olcPixelGameEngine/extensions", "");
+            text = text.replaceAll("/src/third-party/olcPixelGameEngine/utilities", "");
+            text = text.replaceAll("/src/third-party/olcSoundWaveEngine", "");
+            text = text.replaceAll("/src/cache/third-party", "");
+            
+            text = text.replaceAll(workspaceDirectory, "");
+            text = text.replaceAll("./third-party/olcPixelGameEngine", "");
+            text = text.replaceAll("./third-party/olcPixelGameEngine/extensions", "");
+            text = text.replaceAll("./third-party/olcPixelGameEngine/utilities", "");
+            text = text.replaceAll("./third-party/olcSoundWaveEngine", "");
+            
+            text = text.split("\n");
+            let retval = [];
+            for(let i = 0; i < text.length; i++)
+            {
+                if(text[i].indexOf("/pgetinker.cpp") === 0)
+                    retval.push(text[i]);
+            }
+            
+            return retval.join("\n");
+        }
+
+        exec(compileCommand.join(" "), (error, stdout, stderr) =>
         {
             if(error)
             {
-                response.status(400)
-                        .send({
-                            stdout: FilterOutput(stdout),
-                            stderr: FilterOutput(stderr),
-                        });
-
+                reject({
+                    code: 400,
+                    stdout: FilterOutput(stdout),
+                    stderr: FilterOutput(stderr),
+                });
+    
+                // cleanup
                 rmSync(workspaceDirectory, { force: true, recursive: true});
                 return;
             }
-
-            if(existsSync(outputFile))
+            exec(linkCommand.join(" "), (error, stdout, stderr) =>
             {
-                response.status(200)
-                        .send({
-                            html: readFileSync(outputFile, "utf8"),
-                        });
-                
-                rmSync(workspaceDirectory, { force: true, recursive: true});
-                return;
-            }
-
-            response.status(42069)
-                    .send({
-                        statusCode: 42069,
-                        message: "something really fucked up happened to get to this point"
+                if(error)
+                {
+                    reject({
+                        code: 400,
+                        stdout: FilterOutput(stdout),
+                        stderr: FilterOutput(stderr),
                     });
-            
-            rmSync(workspaceDirectory, { force: true, recursive: true});
+        
+                    // cleanup
+                    rmSync(workspaceDirectory, { force: true, recursive: true});
+                    return;
+                }
+    
+                if(existsSync(outputFile))
+                {
+                    resolve({
+                        code: 200,
+                        html: readFileSync(outputFile, "utf8"),
+                    });
+
+                    // cleanup
+                    rmSync(workspaceDirectory, { force: true, recursive: true});
+                    return;
+                }
+    
+                reject({
+                    code: 42069,
+                    message: "something really fucked up happened to get to this point."
+                });
+                
+                // cleanup
+                rmSync(workspaceDirectory, { force: true, recursive: true});
+            });
         });
     });
+
 }
 
 /** @param {Express.Application} app*/
 export default function ApiHandler(app)
 {
-    app.post("/api/compile", Compile);
+    
+    app.post("/api/compile", (request, response) =>
+    {
+        // bail out if we haven't been provided some code to process
+        if(typeof request.body.code === "undefined")
+        {
+            response.status(400)
+                    .send({
+                        message: "missing required parameters"
+                    });
+            return;
+        }
+        
+        Compile(request.body.code)
+            .then((result) =>
+            {
+                response.status(result.code)
+                        .send(result);
+            })
+            .catch((error) =>
+            {
+                response.status(error.code)
+                        .send(error);
+            });
+    });
+
+
 }
