b4962db7ec154b6863ff6532eca83e03701b2ea2
Moros Smith <moros1138@gmail.com>
2024-04-04 02:24:43 -0400

    a whole lot.... basically reorganized UI handlers, added share.. and did away with some stupidity

--- src/client/index.js
+++ src/client/index.js
@@ -8,21 +8,17 @@ import playerPanelTemplate from "./templates/player-panel.html?raw";
 
 class App
 {
+    buttons;
     editor;
     model;
     layout;
     layoutConfig;
     layoutConfigDefault;
     playerLastHtml;
-    self;
     theme;
-    ui;
 
     constructor()
     {
-        // because stupidity
-        self = this;
-        
         // initialize the default layout configuration
         this.layoutConfigDefault = {
             settings: {
@@ -33,22 +29,22 @@ class App
                 content:[{
                     type: 'component',
                     componentName: 'editorComponent',
-                    componentState: function() { return self; },
+                    componentState: {},
                     isClosable: false,
                     title: 'C++ Editor',
                 },{
                     type: 'component',
                     componentName: 'playerComponent',
-                    componentState: function() { return self; },
+                    componentState: {},
                     isClosable: false,
                     title: 'Emscripten Player',
                 }]
             }]
         };
         
-        this.layoutConfig = this.layoutConfigDefault;
-
-        this.ui = [{
+        this.layoutConfig = ((window.localStorage.getItem("pgetinkerLayout") !== null) ? JSON.parse(window.localStorage.getItem("pgetinkerLayout")) : this.layoutConfigDefault);
+        
+        this.buttons = [{
             element: () => { return document.querySelector("#default-code"); },
             callback: (event) =>
             {
@@ -80,7 +76,7 @@ class App
             callback: (event) =>
             {
                 event.preventDefault();
-                alert("TODO: " + event.target.innerHTML);
+                this.Share();
             }
         },{
             element: () => { return document.querySelector("#compile"); },
@@ -95,34 +91,35 @@ class App
 
         this.SetupLayout();
         
-        this.SetTheme("dark");
+        let theme = ((window.localStorage.getItem("pgetinkerTheme") !== null) ? window.localStorage.getItem("pgetinkerTheme") : "dark");
+        theme = (theme === "dark" || theme === "light") ? theme : "dark";
+        
+        this.SetTheme(theme);
     }
 
     SetupLayout()
     {
-        this.layout = new GoldenLayout(this.layoutConfig, document.querySelector("#content"));
 
-        this.layout.registerComponent('playerComponent', function(container, state)
-        {
-            container.getElement().html(`
-                <div id="player-panel">
-                    <iframe src="/player.html"></iframe>
-                    <div></div>
-                </div>
-            `);
-        });
-        
-        this.layout.registerComponent('editorComponent', function (container, state)
+        function EditorComponent(container, state)
         {
             container.getElement().html(editorPanelTemplate);
-            
-            container.on("resize", () =>
-            {
-                if(typeof state.editor === "undefined")
-                    return;
+        }
         
-                state.editor.layout();
-            });
+        function PlayerComponent(container, state)
+        {
+            container.getElement().html(`<div id="player-panel"><iframe src="/player.html"></iframe><div></div></div>`);
+        }
+
+
+        this.layout = new GoldenLayout(this.layoutConfig, document.querySelector("#content"));
+
+        this.layout.registerComponent('editorComponent', EditorComponent);
+        this.layout.registerComponent('playerComponent', PlayerComponent);
+        
+        this.layout.on("stateChanged", () =>
+        {
+            console.log("TESTING 1234");
+            window.localStorage.setItem("pgetinkerLayout", JSON.stringify(this.layout.toConfig()));
         });
 
         this.layout.on("initialised", () =>
@@ -133,7 +130,10 @@ class App
             });
 
             if(typeof this.model === "undefined")
+            {
                 this.model = monaco.editor.createModel("", "cpp", monaco.Uri.parse("inmemory://pgetinker"));
+                this.model.setValue("This is a test");
+            }
 
             this.editor = monaco.editor.create(document.querySelector('#editor-panel .code-editor'), {
                 automaticLayout: true,
@@ -141,9 +141,15 @@ class App
                 theme: `vs-${this.theme}`
             });
             
-            for(let i = 0; i < this.ui.length; i++)
+            
+            this.editor.onDidChangeModelContent(() =>
             {
-                this.ui[i].element().addEventListener("click", this.ui[i].callback);
+                window.localStorage.setItem("pgetinkerCode", JSON.stringify(this.editor.getValue()));
+            });
+
+            for(let i = 0; i < this.buttons.length; i++)
+            {
+                this.buttons[i].element().addEventListener("click", this.buttons[i].callback);
             }
             
             document.querySelector('#player-panel iframe').srcdoc = this.playerLastHtml;
@@ -188,18 +194,31 @@ class App
             this.editor.updateOptions({ theme: `vs-${this.theme}`});
 
         document.querySelector("body").className = this.theme;
-        document.querySelector("#player-panel iframe").contentWindow.document.body.className = this.theme;
+        
+        let interval = setInterval(() =>
+        {
+            if(typeof document.querySelector("#player-panel iframe").contentWindow.document.body.className === "undefined")
+                return;
+            
+            document.querySelector("#player-panel iframe").contentWindow.document.body.className = this.theme;
+            clearInterval(interval);
+        }, 50);
+        
+        window.localStorage.setItem("pgetinkerTheme", this.theme);
     }
 
-    CloseMarkers()
+    BeforeCompile()
     {
+        this.playerLastHtml = playerPanelTemplate;
+        document.querySelector('#player-panel iframe').srcdoc = this.playerLastHtml;
+    
         monaco.editor.removeAllMarkers("owner");
         this.editor.trigger("", "closeMarkersNavigation");
     }
-    
+
     Compile()
     {
-        this.CloseMarkers();
+        this.BeforeCompile();
 
         fetch("/api/compile", {
             method: "POST",
@@ -240,8 +259,61 @@ class App
                 return;
             }
 
-            self.playerLastHtml = result.html;
-            document.querySelector('#player-panel iframe').srcdoc = self.playerLastHtml;
+            this.playerLastHtml = result.html;
+            document.querySelector('#player-panel iframe').srcdoc = this.playerLastHtml;
+    
+        }).catch((error) =>
+        {
+            console.log("Awwww fuck!", error);
+        })
+    }
+
+    Share()
+    {
+        this.BeforeCompile();
+
+        fetch("/api/share", {
+            method: "POST",
+            headers: { "Content-type": "application/json" },
+            body: JSON.stringify({code: this.editor.getValue() }),
+        }).then((response) =>
+        {
+            return response.json();
+        }).then((result) =>
+        {
+            if(result.stderr)
+            {
+                const regex = new RegExp(':(\\d+):(\\d+): (error|warning): (.*)', 'gm')
+                
+                let markers = [];
+                
+                let matches;
+        
+                while((matches = regex.exec(result.stderr)) !== null)
+                {
+                    markers.push({
+                        message: matches[4],
+                        severity: (matches[3] === "warning") ? monaco.MarkerSeverity.Warning : monaco.MarkerSeverity.Error,
+                        startLineNumber: parseInt(matches[1]),
+                        startColumn: parseInt(matches[2]),
+                        endLineNumber: parseInt(matches[1]),
+                        endColumn: this.model.getLineLength(parseInt(matches[1])),
+                        source: "Emscripten",
+                    });            
+                }
+                
+                // show errors in the editor
+                monaco.editor.setModelMarkers(this.model, "owner", markers);
+                this.editor.setPosition({lineNumber: markers[0].startLineNumber, column: markers[0].startColumn });
+                
+                setTimeout(() => { this.editor.trigger("", "editor.action.marker.next"); }, 50);
+                
+                return;
+            }
+            
+            alert("TODO: Share Stuff!");
+            this.playerLastHtml = result.html;
+            document.querySelector('#player-panel iframe').srcdoc = this.playerLastHtml;
     
         }).catch((error) =>
         {
