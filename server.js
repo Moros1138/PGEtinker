26a75e46b34534da34c49b1e97a9945f74f55ce4
Moros Smith <moros1138@gmail.com>
2024-04-05 15:44:25 -0400

    moved api handling to server.js, lots of reorganizing

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 import fs from "node:fs/promises";
-
 import express from "express";
 import morgan from "morgan";
-
 import dotenv from "dotenv";
-import ApiHandler from "./src/server/lib/ApiHandler.js";
+import { createHash } from "node:crypto";
+import { Sequelize, Model, DataTypes } from "sequelize";
+import { Compile, GenerateSlug } from "./src/server/lib/utils.js";
+import { join } from "node:path";
+import { Code, SetupCodeDatabase } from "./src/server/lib/Code.js";
+import { readFileSync } from "node:fs";
 
 // load configuration from .env files
 dotenv.config();
 
 // constants
+
+/** @type {string} - the path to the sqlite database file */
+const sqliteDatabaseFile = process.env.SQLITE_DBFILE || join(process.cwd(), "cache", "data", "database.sqlite");
 
 /** @type {boolean} - are we in production mode or not */
 const isProduction = process.env.NODE_ENV === "production";
@@ -32,6 +38,26 @@ const ssrManifest = isProduction
 
 /** @type {Express} - the express app */
 const app = express();
+
+app.set("trust proxy", true);
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: sqliteDatabaseFile,
+    logging: false
+});
+
+try
+{
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+}
+catch (error)
+{
+    console.error('Unable to connect to the database:', error);
+}
+
+SetupCodeDatabase(sequelize);
 
 /** @type {import("vite").ViteDevServer | undefined} - the instance of the vite development server, if not in production */
 let vite;
@@ -60,7 +86,146 @@ else
 app.use(morgan("tiny"));
 app.use("/api", express.json());
 
-ApiHandler(app);
+app.post("/api/compile", (request, response) =>
+{
+    // bail out if we haven't been provided some code to process
+    if(typeof request.body.code === "undefined")
+    {
+        response.status(400)
+                .send({
+                    message: "missing required parameters"
+                });
+        return;
+    }
+    
+    Compile(request.body.code)
+        .then((result) =>
+        {
+            response.status(result.code)
+                    .send(result);
+        })
+        .catch((error) =>
+        {
+            response.status(error.code)
+                    .send(error);
+        });
+});
+
+app.get("/api/code/:codeSlug", (request, response) =>
+{
+    if(typeof request.params.codeSlug === "undefined")
+    {
+        response.status(400)
+                .send({
+                    status: 400,
+                    message: "missing required parameters"
+                });
+        return;
+    }
+    
+    (async() =>
+    {
+        const code = await Code.findOne({ where: { slug: request.params.codeSlug }});
+        if(code === null)
+        {
+            response.status(404)
+                    .send({
+                        status: 404,
+                        message: "not found"
+                    });
+            return;
+        }
+
+        response.status(200)
+                .send({
+                    status: 200,
+                    code: code.code
+                });
+    })();
+});
+
+app.post("/api/code", (request, response) =>
+{
+    // bail out if we haven't been provided some code to process
+    if(typeof request.body.code === "undefined")
+    {
+        response.status(400)
+                .send({
+                    message: "missing required parameters"
+                });
+        return;
+    }
+    
+    const hash = createHash("sha256");
+
+    hash.update(request.body.code);
+    
+    let hashedCode = hash.digest("hex");
+
+    Compile(request.body.code)
+        .then((result) =>
+        {
+            // successfully compiled, let's store the code
+            (async() =>
+            {
+                const [code, created] = await Code.findOrCreate({
+                    where: {
+                        hashedCode,
+                    },
+                    defaults: {
+                        code: request.body.code,
+                        slug: "==not-set==",
+                    }
+                });
+                    
+                if(code.slug === "==not-set==")
+                {
+                    let keepTrying = true;
+                    while(keepTrying)
+                    {
+                        const slug = GenerateSlug();
+                        const slugFinder = await Code.findOne({ where: { slug }});
+                        if(slugFinder === null)
+                        {
+                            keepTrying = false;
+                            code.slug = slug;
+                            code.save();
+                        }
+                    }
+                }
+                
+                response.status(200)
+                        .send({
+                            slug: `${request.protocol}://${request.host}/s/${code.slug}`,
+                            html: result.html,
+                        });
+            
+            })();
+        })
+        .catch((error) =>
+        {
+            response.status(error.code)
+                    .send(error);
+        });
+});
+
+app.get("/goldenlayout-base.css", (request, response) =>
+{
+    const goldenLayoutBase = readFileSync("node_modules/golden-layout/src/css/goldenlayout-base.css");
+    response.status(200).set({"Content-type": "text/css"}).send(goldenLayoutBase);
+});
+
+app.get("/goldenlayout-dark.css", async(request, response) =>
+{
+    const goldenLayoutTheme = readFileSync("node_modules/golden-layout/src/css/goldenlayout-dark-theme.css");
+    response.status(200).set({"Content-type": "text/css"}).send(goldenLayoutTheme);
+});
+
+app.get("/goldenlayout-light.css", async(request, response) =>
+{
+    const goldenLayoutTheme = readFileSync("node_modules/golden-layout/src/css/goldenlayout-light-theme.css");
+    response.status(200).set({"Content-type": "text/css"}).send(goldenLayoutTheme);
+});
 
 app.use("*", async(request, response) =>
 {
@@ -68,7 +233,7 @@ app.use("*", async(request, response) =>
     {
         /** @type {string} - url, stripped of the base, for uses in path matching */
         const url = request.originalUrl.replace(base, '');
-
+        
         /** @type {string | undefined} - the text template used by the rendering function */
         let template;
         /** @type {function | undefined} - placeholder for the rendering function */
@@ -88,13 +253,32 @@ app.use("*", async(request, response) =>
             render = (await import("./dist/server/index.js")).render;
         }
 
+        const shareSlug = (url.indexOf("s/") === 0) ? url.replace("s/", "") : null;
+        let    slugCode = "";
+
+        if(shareSlug)
+        {
+            const code = await Code.findOne({ where: { slug: shareSlug }});
+
+            if(code === null)
+            {
+                response.redirect("/");
+                return;
+            }
+
+            slugCode  = '<script id="slug-code">let slugCode = String.raw`';
+            slugCode += String.raw`${code.code}`;
+            slugCode += '`;</script>';
+        }
+
         // do the actual rendering
         const rendered = await render(url, ssrManifest);
         
         // apply the rendering to the template
         const html = template
             .replace(`<!--app-head-->`, rendered.head ?? "")
-            .replace(`<!--app-html-->`, rendered.html ?? "");
+            .replace(`<!--app-html-->`, rendered.html ?? "")
+            .replace(`<!--slug-code-->`, slugCode);
         
         // I got something to say. it's better to burn out than fade away!
         response.status(200).set({"Content-Type": "text/html"}).send(html);
--- src/server/index.js
+++ src/server/index.js
@@ -1,52 +1,8 @@
-import goldenLayoutBaseStyle from "golden-layout/src/css/goldenlayout-base.css?raw";
 
 export function render()
 {
-    const head = `
-        <meta name="description" value="Interactively develop an olc::PixelGameEngine program from your Browser!">
-
-        <meta property="og:title" content="PGEtinker">
-        <meta property="og:description" content="Interactively develop an olc::PixelGameEngine program from your Browser!">
-        <meta property="og:type" content="website">
-
-        <link rel="stylesheet" type="text/css" href="/style.css">
-        <style>${goldenLayoutBaseStyle}</style>
-        <style id="goldenlayout-theme"></style>
-    `;
-
-
-    const html = `
-        <div id="header">
-            <a href="/">
-                <img src="/images/PGEtinker-logo.png" alt="PGEtinker Logo">
-            </a>
-            <div class="branding">
-                <a href="https://github.com/OneLoneCoder/olcPixelGameEngine" target="_blank">
-                    <img id="olc-brand" src="/images/pge-logo.png" alt="OneLoneCoder PixelGameEngine Logo">
-                </a>
-                <a href="https://emscripten.org/" target="_blank">
-                    <img id="emscripten-brand" src="/images/emscripten-logo.png" alt="Emscripten Logo">
-                </a>
-            </div>
-        </div>
-        <div id="content">
-            <noscript>
-                <p>
-                    It's cute that you think you're going to use a
-                    webapp that relies so heavily on javascript...
-                    ...without javascript.
-                </p>
-                <p>
-                    If your browser is capable of using javascript yet you still
-                    see this message, please consider using a real browser like
-                    Firefox and Chromium (or Chromium forks).
-                </p>
-            </noscript>
-        </div>
-        <div id="footer">
-            Haven't quite decided what should go here, probably a copyright notice or perhaps a whitty quote.
-        </div>
-    `;
+    const head = ``;
+    const html = ``;
 
     return {
         html,
