e257497ebd0b42d3fd024ed730a85d719f81140f
Moros Smith <moros1138@gmail.com>
2024-04-19 07:32:29 -0400

    remove comment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,7 +20,6 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
         
         $middleware->remove([
-            // ConvertEmptyStringsToNull::class,
             TrimStrings::class,
         ]);
     })
