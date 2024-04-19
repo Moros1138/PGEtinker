0f1a27a037f8b1611a1912810e455f16881d3963
Moros Smith <moros1138@gmail.com>
2024-04-19 14:02:59 -0400

    fix storage permissions

--- config/filesystems.php
+++ config/filesystems.php
@@ -33,6 +33,16 @@ return [
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
+            'permissions' => [
+                "file" => [
+                    "public" => 0644,
+                    "private" => 0644,
+                ],
+                "dir" => [
+                    "public" => 0755,
+                    "private" => 0755,
+                ],
+            ],
             'throw' => false,
         ],
 
