8fdb0145f7bec43cd595b4ee17b763708bb6b592
Moros Smith <moros1138@gmail.com>
2024-04-22 18:27:03 -0400

    backend provides the model

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,17 @@ use Illuminate\Support\Facades\Route;
 Route::post("/share",   [CodeController::class, "Share" ]);
 Route::post("/compile", [CodeController::class, "Compile" ]);
 
+Route::get("model/{version}", function(Request $request, string $version)
+{
+    if(file_exists(base_path() . "/third_party/{$version}/model.h"))
+        return file_get_contents(base_path() . "/third_party/{$version}/model.h");
+    
+    return response([
+        "statusCode" => 404,
+        "message" => "Model Version {$version} not found.",
+    ], 404);
+});
+
 Route::get("/default-code", function(Request $request)
 {
     return [
