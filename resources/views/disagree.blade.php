1692b23de2ad50f3fb70520619b39c5ea6368579
Moros Smith <moros1138@gmail.com>
2024-04-24 20:31:52 -0400

    add disagree route/view

---
+++ resources/views/disagree.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>You disagreed | PGEtinker</title>
+    <link rel="icon" type="text/svg" href="/favicon.svg">
+    
+    <link rel="stylesheet" type="text/css" href="{{ Vite::asset('resources/css/disagreed.scss')}}">
+</head>
+<body>
+
+</body>
+</html>
