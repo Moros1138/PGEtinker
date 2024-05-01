c5e88ac2ce6708c136101b913049de64626b6f5d
Moros Smith <moros1138@gmail.com>
2024-05-01 14:46:22 -0400

    add version to bug reporting

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -83,7 +83,7 @@
                             <i data-lucide="github"></i>
                             PGEtinker on Github
                         </a>
-                        <a target="_blank" href="https://github.com/Moros1138/PGEtinker/issues/new?assignees=Moros1138&labels=bug&projects=&template=bug_report.md&title=">
+                        <a target="_blank" href="https://github.com/Moros1138/PGEtinker/issues/new?assignees=Moros1138&labels=bug&projects=&template=bug_report.md&title=Bug in Version: {{ env('VERSION', 'develop') }}">
                             <i data-lucide="bug"></i>
                             Report an issue
                         </a>
