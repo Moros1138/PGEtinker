4f928a58f4da54d9a28219d6e5da98346198752c
Moros Smith <moros1138@gmail.com>
2024-05-02 10:10:23 -0400

    doesn't belong in dockerfile, belongs in entrypoint

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
 
 # refresh the cache at startup
-su -c "php artisan config:cache" -s /bin/bash www-data
-su -c "php artisan route:cache" -s /bin/bash www-data
+su -c "php artisan config:clear" -s /bin/bash www-data
+su -c "php artisan cache:clear" -s /bin/bash www-data
+su -c "php artisan view:clear" -s /bin/bash www-data
+su -c "php artisan route:clear" -s /bin/bash www-data
 
 # the server
 apache2-foreground
