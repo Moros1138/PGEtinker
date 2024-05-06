5b0aa00e8b6bb6c6eaf990d6858297bf78a917d0
Moros Smith <moros1138@gmail.com>
2024-05-06 10:35:43 -0400

    change class name of new console command

--- app/Console/Commands/Patreon.php
+++ app/Console/Commands/Patreon.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
-class MoveDatabase extends Command
+class Patreon extends Command
 {
     /**
      * The name and signature of the console command.
