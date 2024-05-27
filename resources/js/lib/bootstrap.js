dd77e132baa7f9723b25a28cc074a46c0e7da628
Moros Smith <moros1138@gmail.com>
2024-05-26 23:52:13 -0400

    remove beta subpath, not going to use it

--- resources/js/lib/bootstrap.js
+++ resources/js/lib/bootstrap.js
@@ -6,11 +6,6 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios.defaults.withCredentials = true;
 window.axios.defaults.withXSRFToken = true;
 
-if(window.location.pathname.indexOf("/beta/") == 0)
-{
-    window.axios.defaults.baseURL = `${window.location.protocol}//${window.location.host}/staging/`;
-}
-
 if(window.location.pathname.indexOf("/staging/") == 0)
 {
     window.axios.defaults.baseURL = `${window.location.protocol}//${window.location.host}/staging/`;
