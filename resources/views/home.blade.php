6cc1c4a78c3f79bb5c317e4e3b227e7a30aa4602
Moros Smith <moros1138@gmail.com>
2024-06-02 19:37:21 -0400

    add progress web application

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="manifest" href="manifest.json">
     
     <meta property="twitter:card" content="summary_large_image" />
     <meta property="twitter:title" content="PGEtinker" />
