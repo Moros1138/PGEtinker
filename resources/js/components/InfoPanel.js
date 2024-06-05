8fe914020b11061b3fbcd2dd440fc48b35502620
Moros Smith <moros1138@gmail.com>
2024-06-05 16:13:12 -0400

    wip problems in the #info-panel

--- resources/js/components/InfoPanel.js
+++ resources/js/components/InfoPanel.js
@@ -7,6 +7,39 @@ export default class InfoPanel
     {
         this.state = state;
         console.log("Info panel", "constructor");
+        
+        window.addEventListener("update-problems-panel", (event) =>
+        {
+            const diagnostics = event.detail;
+
+            document.querySelector("#info-panel").innerHTML = "";
+            const wrapper = document.createElement("div");
+
+            diagnostics.forEach((diagnostic) =>
+            {
+                const container = document.createElement("p");
+                const link      = document.createElement("a");
+
+                link.setAttribute("href", "#");
+                link.setAttribute("data-line-number", diagnostic.range.start._line);
+                link.setAttribute("data-column", diagnostic.range.start._character);
+                link.setAttribute("title", diagnostic.message);
+                link.innerHTML = diagnostic.message;
+
+                link.addEventListener("click", (event) =>
+                {
+                    const currentLink = event.target;
+                    const lineNumber = parseInt(currentLink.getAttribute("data-line-number")) + 1;
+                    const column     = parseInt(currentLink.getAttribute("data-column")) + 1;
+                    this.state.editorPanel.reveal({ lineNumber, column });
+                });
+                
+                container.append(link);
+                wrapper.append(container);
+            });
+            
+            document.querySelector("#info-panel").append(wrapper);
+        });
     }
 
 
@@ -43,9 +76,22 @@ export default class InfoPanel
 
     setContent(content)
     {
-        let infoPanel = document.querySelector("#info-panel");
-        infoPanel.innerHTML = `<div>${content}</div>`;
-        infoPanel.scrollTop = infoPanel.scrollHeight;
+        const infoPanel = document.querySelector("#info-panel");
+        
+        while(infoPanel.lastElementChild)
+        {
+            infoPanel.removeChild(infoPanel.lastElementChild);
+        }
+        
+        if(typeof content === "string")
+        {
+            infoPanel.innerHTML = `<div>${content}</div>`; 
+        }
+        else
+        {
+            infoPanel.append(content);
+        }
+            
     }
 }
 
