adab46975a699298be0e8146e7d9933fef040f01
Moros Smith <moros1138@gmail.com>
2024-06-08 02:51:29 -0400

    default layouts for portrait and landscape modes

---
+++ resources/js/lib/defaultLandscapeLayout.js
@@ -0,0 +1,54 @@
+const defaultLandscapeLayout = {
+    settings: {
+        showPopoutIcon: false,
+    },
+    content: [{
+        type: 'column',
+        content:[{
+            type: 'row',
+            content:[{
+                type: 'component',
+                componentName: 'editorComponent',
+                componentState: {},
+                isClosable: false,
+                title: 'C++ Editor',
+                id: 'editor',
+            },{
+                type: 'component',
+                componentName: 'playerComponent',
+                componentState: {},
+                isClosable: false,
+                title: 'Emscripten Player',
+                id: 'player',
+            }],
+        },{
+            type: 'stack',
+            id: 'information-stack',
+            height: 25,
+            content: [{
+                type: 'component',
+                componentName: 'problemsComponent',
+                componentState: {},
+                isClosable: false,
+                title: 'Problems',
+                id: 'problems',
+            },{
+                type: 'component',
+                componentName: 'compilerOutputComponent',
+                componentState: {},
+                isClosable: false,
+                title: 'Compiler Output',
+                id: 'compiler-output',
+            },{
+                type: 'component',
+                componentName: 'consoleComponent',
+                componentState: {},
+                isClosable: false,
+                title: 'Console',
+                id: 'console',
+            }]
+        }],
+    }],
+};
+
+export default defaultLandscapeLayout;
