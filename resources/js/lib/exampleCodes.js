9d3e52c68652f4e3b258b86ee7437a05120538d7
Moros Smith <moros1138@gmail.com>
2024-06-26 22:24:10 -0400

    add example codes

---
+++ resources/js/lib/exampleCodes.js
@@ -0,0 +1,17 @@
+import code1 from '../../example1.cpp?raw';
+import code2 from '../../example2.cpp?raw';
+import code3 from '../../example3.cpp?raw';
+import code4 from '../../example4.cpp?raw';
+import code5 from '../../example5.cpp?raw';
+import code6 from '../../example6.cpp?raw';
+
+const examples = {
+    code1: { code: code1, label: "Example 1", },
+    code2: { code: code2, label: "Example 2", },
+    code3: { code: code3, label: "Example 3", },
+    code4: { code: code4, label: "Example 4", },
+    code5: { code: code5, label: "Example 5", },
+    code6: { code: code6, label: "Example 6", },
+};
+
+export default examples;
