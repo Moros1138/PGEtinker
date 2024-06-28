530c70cd047f34e2d088c364a46cd4fe6ece1df3
Moros Smith <moros1138@gmail.com>
2024-06-28 01:38:51 -0400

    don't add  metadata to the example code loader

--- resources/js/lib/exampleCodes.js
+++ resources/js/lib/exampleCodes.js
@@ -6,12 +6,12 @@ import code5 from '../../example5.cpp?raw';
 import code6 from '../../example6.cpp?raw';
 
 const examples = {
-    code1: { code: code1, label: "Example 1", },
-    code2: { code: code2, label: "Example 2", },
-    code3: { code: code3, label: "Example 3", },
-    code4: { code: code4, label: "Example 4", },
-    code5: { code: code5, label: "Example 5", },
-    code6: { code: code6, label: "Example 6", },
+    code1,
+    code2,
+    code3,
+    code4,
+    code5,
+    code6,
 };
 
 export default examples;
