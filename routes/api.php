809e0dc34fd7e4dabba106eb6dbb302d21899836
Moros Smith <moros1138@gmail.com>
2024-06-28 07:40:30 -0400

    remove model and tests pertaining to it, obselete since LSP

--- routes/api.php
+++ routes/api.php
@@ -9,17 +9,6 @@ Route::post("/share",   [CodeController::class, "Share" ]);
 Route::post("/compile", [CodeController::class, "Compile" ]);
 Route::get("/health-check", [CodeController::class, "HealthCheck" ]);
 
-Route::get("model/{version}", function(Request $request, string $version)
-{
-    if(file_exists(base_path() . "/third_party/{$version}/model.h"))
-        return file_get_contents(base_path() . "/third_party/{$version}/model.h");
-    
-    return response([
-        "statusCode" => 404,
-        "message" => "Model Version {$version} not found.",
-    ], 404);
-});
-
 Route::get("/news", function(Request $request)
 {
     $changeLog = new stdClass();
