2796394ee3d3440adf9487acf52a782bf48b6442
Moros Smith <moros1138@gmail.com>
2024-07-12 12:53:41 -0400

    set csrf cookie before calling /api/news

--- resources/js/lib/newsDialog.js
+++ resources/js/lib/newsDialog.js
@@ -8,45 +8,48 @@ export default function newsDialog()
         dialog.classList.toggle("dialog", true);
         dialog.classList.toggle("news", true);
         
-        axios.get("/api/news").then((response) =>
+        axios.get('/sanctum/csrf-cookie').then(_ =>
         {
-            
-            let entries = [];
-            
-            response.data.entries.forEach((entry) =>
+            axios.get("/api/news").then((response) =>
             {
-                entries.push(`<div class="${entry.type}">${entry.message}</div>`);
+                
+                let entries = [];
+                
+                response.data.entries.forEach((entry) =>
+                {
+                    entries.push(`<div class="${entry.type}">${entry.message}</div>`);
+                });
+    
+                dialog.innerHTML = `
+                <div class="window">
+                    <div class="header">News and Updates</div>
+                    <div class="content">
+                        <h3>PGEtinker has been updated. Here's what's been done:</h3>
+                        <h3>${response.data.date}</h3>
+                        <div class="entries">${entries.join("")}</div>
+                        
+                        <p>
+                            For more details visit the <a href="https://github.com/Moros1138/PGEtinker" target="_blank">PGEtinker github repository</a>.
+                        </p>
+                        <p>
+                        Version: ${version.substring(0, 7)}
+                        </p>
+                    </div>
+                    <div class="footer">
+                        <button class="ok">Close</button>
+                    </div>
+                </div>`;
+    
+                dialog.querySelector("button.ok").addEventListener("click", (event) =>
+                {
+                    dialog.remove();
+                    resolve();
+                });
+                
+                document.body.appendChild(dialog);
             });
-
-            dialog.innerHTML = `
-            <div class="window">
-                <div class="header">News and Updates</div>
-                <div class="content">
-                    <h3>PGEtinker has been updated. Here's what's been done:</h3>
-                    <h3>${response.data.date}</h3>
-                    <div class="entries">${entries.join("")}</div>
-                    
-                    <p>
-                        For more details visit the <a href="https://github.com/Moros1138/PGEtinker" target="_blank">PGEtinker github repository</a>.
-                    </p>
-                    <p>
-                    Version: ${version.substring(0, 7)}
-                    </p>
-                </div>
-                <div class="footer">
-                    <button class="ok">Close</button>
-                </div>
-            </div>`;
-
-            dialog.querySelector("button.ok").addEventListener("click", (event) =>
-            {
-                dialog.remove();
-                resolve();
-            });
-            
-            document.body.appendChild(dialog);
-        });
-        
+    });
+       
     });
 
 }
